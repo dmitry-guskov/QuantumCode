17e009819b9883fa2fb77e084166047935f58181
dimav <dimavg200>
2022-06-27 13:35:30 +0300

    Adding example.ipynb with using ChannelTomography as ChT package.

--- ChannelTomography.py
+++ ChannelTomography.py
@@ -37,9 +37,9 @@ class ChannelProc:
         return expected
 
 
-# for curcuit and beckend return chennel ( works only for teleoprtation-like systems)
-def channel_from_proc(backend, circuit, noise_model=None, measured=1, prepeared=0, input_job=None):
-    qpt_circs = process_tomography_circuits(circuit, [measured], prepared_qubits=[prepeared])
+# for circuit and backend return channel ( works only for teleportation-like systems)
+def channel_from_proc(circuit, backend, noise_model=None, measured=1, prepared=0, input_job=None):
+    qpt_circs = process_tomography_circuits(circuit, [measured], prepared_qubits=[prepared])
     if input_job is None:
         job = qiskit.execute(qpt_circs, backend, shots=4000, noise_model=noise_model)
     else:
@@ -62,7 +62,7 @@ def channel_from_proc(backend, circuit, noise_model=None, measured=1, prepeared=
     return chan, job
 
 
-def final_state_tomography(backend, circuit, noise_model=None, measured=1, prepeared=0, input_job=None):
+def final_state_tomography(circuit, backend, noise_model=None, measured=1, prepared=0, input_job=None):
     # Generate circuits and run on simulator
     #     t = time.time()
 
@@ -78,20 +78,20 @@ def final_state_tomography(backend, circuit, noise_model=None, measured=1, prepe
 
     # Perform the tomography fit
     # which outputs a density matrix
-    rho_fit_telep = circ_fitter_teleport.fit(method='lstsq')
-    rho = qi.DensityMatrix(rho_fit_telep)
+    rho_fit_teleport = circ_fitter_teleport.fit(method='lstsq')
+    rho = qi.DensityMatrix(rho_fit_teleport)
     plot_bloch_multivector(rho.data)
     return rho, job
 
 
-def trans_sphere_from_channel(channel, in_States=None, noise_model=None, measured=4):
+def trans_sphere_from_channel(channel, in_states=None, noise_model=None, measured=4):
     # Create a sphere
     u, v = np.mgrid[0:2 * np.pi:12j, 0:np.pi:16j]
     x = np.cos(u) * np.sin(v)
     y = np.sin(u) * np.sin(v)
     z = np.cos(v)
 
-    if in_States is None:
+    if in_states is None:
         xx = np.copy(x)
         yy = np.copy(y)
         zz = np.copy(z)
@@ -107,7 +107,7 @@ def trans_sphere_from_channel(channel, in_States=None, noise_model=None, measure
     else:
         xx, yy, zz = np.array([]), np.array([]), np.array([])
         in_x, in_y, in_z = [], [], []
-        for in_state in in_States:
+        for in_state in in_states:
             sub_rho = channel.outvec(in_state)
             #             sub_rho = qi.DensityMatrix(in_state)
             xx = np.append(xx, (qi.Pauli('X').to_matrix().dot(sub_rho)).trace().real)
@@ -126,7 +126,7 @@ def trans_sphere_from_channel(channel, in_States=None, noise_model=None, measure
     #     ax.plot_wireframe(xx, yy, zz, color="k", rstride=1, cstride=2,linewidth=1)
     ax.scatter(xx, yy, zz, color="k", s=1)
     #     ax.plot( xx, yy, zz, color='c', alpha=0.3, linewidth=1)
-    if in_States is not None:
+    if in_states is not None:
         ax.scatter(in_x, in_y, in_z, color="b", s=1)
     else:
         ax.plot_wireframe(x, y, z, color="r", rstride=1, cstride=2, linewidth=1)
@@ -148,3 +148,18 @@ def prep_states(theta, phi):
         for phi_i in np.exp(1j * phi_arr):
             test_states.append([phi_i * np.cos(a_i), np.sin(a_i)])
     return test_states
+
+
+def counts_to_state_vec(counts):
+    state = [np.sqrt(counts['0'] * 1), np.sqrt(counts['1'] * 1)]
+
+    state = state / np.sqrt(counts['0'] + counts['1'])
+    state = state.tolist()
+    return state
+
+
+def coord_from_matrix(state):
+    x = (qi.Pauli('X').to_matrix().dot(state)).trace().real
+    y = (qi.Pauli('Y').to_matrix().dot(state)).trace().real
+    z = (qi.Pauli('Z').to_matrix().dot(state)).trace().real
+    return [x, y, z]
